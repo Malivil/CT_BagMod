122ca708018816e91cb2d925196b6cecd6be3d3b
mlebson <Malivil@users.noreply.github.com>
2012-11-20 13:28:01 +0000

    MCP for 1.4.5
    Updated TR and SS to 1.4.5

---
+++ trunk/MCP 7.22/src/minecraft/metadark/mods/SuperSlopes/Slopes/ItemSlopes.java
@@ -0,0 +1,54 @@
+package metadark.mods.SuperSlopes.Slopes;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.ItemBlock;
+import net.minecraft.src.ItemStack;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
+
+public class ItemSlopes extends ItemBlock {
+    private static final String[] Kaevator_Materials = new String[] {"StairsUp", "StairsUp", "StairsUp", "StairsUp", "SlopeDown", "SlopeDown", "SlopeDown", "SlopeDown", "SlopeUp", "SlopeUp", "SlopeUp", "SlopeUp", "SlopeSide", "SlopeSide", "SlopeSide", "SlopeSide"};
+    private static final String[] Vanilla_Stair_Materials = new String[] {"Wood", "Sandstone", "Cobblestone", "Bricks", "StoneBrick", "NetherBrick"};
+    private String material;
+
+    public ItemSlopes(final int itemID, final String material) {
+        super(itemID);
+        this.material = material;
+        this.setMaxDamage(0);
+        this.setHasSubtypes(true);
+    }
+
+    @Override
+    public int getMetadata(final int meta) {
+        return meta;
+    }
+
+    @Override
+    public String getItemNameIS(final ItemStack item) {
+        return "Kaev" + this.material + "Slopes" + Kaevator_Materials[item.getItemDamage()];
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return CreativeTabs.tabBlock;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void getSubItems(final int itemID, final CreativeTabs tabs, final List items) {
+        // Don't add stair sub-items for materials that already have vanilla stairs
+        if (Slopes.props.getBoolean("BlockInversion") && !Arrays.asList(Vanilla_Stair_Materials).contains(this.material))
+            items.add(new ItemStack(itemID, 1, 0));
+
+        items.add(new ItemStack(itemID, 1, 4));
+
+        if (Slopes.props.getBoolean("BlockInversion"))
+            items.add(new ItemStack(itemID, 1, 8));
+
+        items.add(new ItemStack(itemID, 1, 12));
+    }
+}
