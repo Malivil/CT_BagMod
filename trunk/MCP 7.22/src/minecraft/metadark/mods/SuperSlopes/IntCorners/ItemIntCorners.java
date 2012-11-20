90f16a3363dea7d6b1db5a01ee598022d6222d57
mlebson <mlebson@579ffad3-307e-43c9-9135-a7ca58636ece>
2012-11-20 13:28:01 +0000

    MCP for 1.4.5
    Updated TR and SS to 1.4.5

---
+++ trunk/MCP 7.22/src/minecraft/metadark/mods/SuperSlopes/IntCorners/ItemIntCorners.java
@@ -0,0 +1,52 @@
+package metadark.mods.SuperSlopes.IntCorners;
+
+import java.util.List;
+
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.ItemBlock;
+import net.minecraft.src.ItemStack;
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
+
+public class ItemIntCorners extends ItemBlock
+{
+    private static final String[] Kaevator_Materials = new String[] {"TruncCornerDown", "TruncCornerDown", "TruncCornerDown", "TruncCornerDown", "TruncCornerUp", "TruncCornerUp", "TruncCornerUp", "TruncCornerUp", "InvCornerDown", "InvCornerDown", "InvCornerDown", "InvCornerDown", "InvCornerUp", "InvCornerUp", "InvCornerUp", "InvCornerUp"};
+    private String material;
+
+    public ItemIntCorners(final int itemID, final String material) {
+        super(itemID);
+        this.material = material;
+        this.setMaxDamage(0);
+        this.setHasSubtypes(true);
+    }
+
+    @Override
+    public int getMetadata(final int meta) {
+        return meta;
+    }
+
+    @Override
+    public String getItemNameIS(final ItemStack item) {
+        return "Kaev" + this.material + "Slopes" + Kaevator_Materials[item.getItemDamage()];
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return CreativeTabs.tabBlock;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void getSubItems(final int itemID, final CreativeTabs tabs, final List items) {
+        items.add(new ItemStack(itemID, 1, 0));
+
+        if (IntCorners.props.getBoolean("BlockInversion"))
+            items.add(new ItemStack(itemID, 1, 4));
+
+        items.add(new ItemStack(itemID, 1, 8));
+
+        if (IntCorners.props.getBoolean("BlockInversion"))
+            items.add(new ItemStack(itemID, 1, 12));
+    }
+}
